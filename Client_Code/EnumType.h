8fe2d57f3a372ff115b3d623eedf3ec909696935
LassieFan <36887763+LassieFan@users.noreply.github.com>
2019-04-07 15:47:01 +0800

    枚举各个功能用数字表示

--- Client_Code/EnumType.h
+++ Client_Code/EnumType.h
@@ -2,7 +2,6 @@
     > File Name: EnumType.h
     > Created Time: Thu 29 Nov 2018 09:09:37 PM CST
  ************************************************************************/
-
 #ifndef __ENUMTYPE_H
 #define __ENUMTYPE_H
 #include<iostream>
